88430d2f606ffeefe04bfb473c8f40c46a02c0e1
Tomasz <tomasz.j.pecyna@gmail.com>
2020-02-21 20:41:59 +0100

    add class wrappers and basic uciserver config reading

--- src/client.py
+++ src/client.py
@@ -1,45 +1,69 @@
 import chess
 import chess.engine
+
 import socket
+
 import pickle
-from easydict import EasyDict as edict
 import sys
+import argparse
+
+from easydict import EasyDict as edict
 
 
-#dunno what these flags mean - I've taken them from some random tutorial.
-client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-
-
-board = chess.Board("r4rk1/pp5p/2p2ppB/3pP3/2P2Q2/P1N2P2/1q4PP/n4R1K w - - 0 21")
-limit = chess.engine.Limit(depth=10)
-
-#funny javascript dicktionary ;D
-client_dick = edict()
-client_dick.size = 0
-
-client_dick.board = board
-client_dick.limit = limit
-#we put size here because we want to know when the server sould stop reading
-client_dick.size = sys.getsizeof(client_dick)
-
-#socket connections need bytes to be send
-serialized = pickle.dumps(client_dick)
-client.connect(('127.0.0.1', 8080))
-client.send(serialized)
-from_serv = bytes()
-
-from_server = bytes()
-while True:
-    #TODO think of adding timeout
-    data = client.recv(4096)
-    from_server += data
-    deserialized = pickle.loads(from_server)
-    #and again, we check if we've read everything
-    try:
-        if deserialized.size == sys.getsizeof(deserialized):
-            break
-    except AttributeError:
+class Client():
+    def __init__(self):
+        # dunno what these flags mean - I've taken them from some random tutorial.
+        self.client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
         pass
-client.close()
-print(deserialized)
 
+    def send(self, args):
+        # TODO argparse here?
+        board = chess.Board("r4rk1/pp5p/2p2ppB/3pP3/2P2Q2/P1N2P2/1q4PP/n4R1K w - - 0 21")
+        limit = chess.engine.Limit(depth=10)
+
+        # funny javascript dictionary ;D
+        client_dick = edict()
+        client_dick.size = 0
+
+        client_dick.board = board
+        client_dick.limit = limit
+        # we put size here because we want to know when the server sould stop reading
+        client_dick.size = sys.getsizeof(client_dick)
+
+        # socket connections need bytes to be send
+        serialized = pickle.dumps(client_dick)
+        self.client.connect(('127.0.0.1', 8080))
+        self.client.send(serialized)
+
+    def read_and_close(self):
+        from_server = bytes()
+        while True:
+            # TODO think of adding timeout
+            data = self.client.recv(4096)
+            from_server += data
+            deserialized = pickle.loads(from_server)
+            # and again, we check if we've read everything
+            try:
+                if deserialized.size == sys.getsizeof(deserialized):
+                    break
+            except AttributeError:
+                pass
+        self.client.close()
+        return deserialized
+
+
+def main(args):
+    client = Client()
+    client.send(args)
+    data = client.read_and_close()
+    print(data)
+
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser()
+
+    # TODO add argparse arguments here
+
+    args, _ = parser.parse_known_args()
+
+    main(args)
--- src/server.py
+++ src/server.py
@@ -1,46 +1,85 @@
 import chess.engine
-import socket
 import chess
+
+import socket
+
 import pickle
 import sys
+import json
+import argparse
+
 from easydict import EasyDict as edict
+from urllib.parse import urlparse
 
-PATH_TO_STOCKFISH = '.\..\stockfish-11-win\stockfish-11-win\Windows\stockfish_20011801_x64.exe'
+
+class Server():
+    def __init__(self, config):
+        self.config = config
+        self.serv = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        parsed = urlparse(config['url'])
+        self.serv.bind((parsed.hostname, parsed.port))
+        # dunno what this listen(5) does.
+        self.serv.listen(5)
+
+    def read(self, conn):
+        from_client = bytes()
+        while True:
+            # TODO think of adding timeout
+            data = conn.recv(4096)
+            from_client += data
+            deserialized = pickle.loads(from_client)
+            # checking if we've read everything
+            try:
+                if deserialized.size == sys.getsizeof(deserialized):
+                    break
+            except AttributeError:
+                pass
+        # received message will be actually a dicktionary
+        client_dick = deserialized
+        return client_dick
+
+    def infere(self, args, data):
+        # TODO use self.config here to setup the engine
+        engine = chess.engine.SimpleEngine.popen_uci(args.stockfish_path)
+        res = engine.play(data.board, data.limit)
+        engine.quit()
+        return res
+
+    def reply(self, res, conn):
+        serv_dick = edict()
+        serv_dick.size = 0
+
+        serv_dick.res = res
+        # and again, we put size of our dictionary so does client know when to stop reading
+        serv_dick.size = sys.getsizeof(serv_dick)
+
+        serialized = pickle.dumps(serv_dick)
+        conn.send(serialized)
+
+    def run(self, args):
+
+        while True:
+            conn, addr = self.serv.accept()
+            data = self.read(conn)
+            res = self.infere(args, data)
+            # TODO do we filter the moves on the server side or on the client side?
+            self.reply(res, conn)
+            conn.close()
 
 
-serv = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-serv.bind(('127.0.0.1', 8080))
+def main(args):
+    with open(args.config_path) as f:
+        config = json.load(f)
+    server = Server(config)
+    server.run(args)
 
-#dunno what this listen(5) does.
-serv.listen(5)
-while True:
-    conn, addr = serv.accept()
-    from_client = bytes()
-    while True:
-        #TODO think of adding timeout
-        data = conn.recv(4096)
-        from_client += data
-        deserialized = pickle.loads(from_client)
-        #checking if we've read everything
-        try:
-            if deserialized.size == sys.getsizeof(deserialized):
-                break
-        except AttributeError:
-            pass
-    #received message will be actually a dicktionary
-    client_dick = deserialized
 
-    engine = chess.engine.SimpleEngine.popen_uci(PATH_TO_STOCKFISH)
-    res = engine.play(client_dick.board, client_dick.limit)
-    engine.quit()
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--config-path', type=str, default='./../uciServer.json')
+    parser.add_argument('--stockfish-path', type=str,
+                        default='./../stockfish-11-win/stockfish-11-win/Windows/stockfish_20011801_x64.exe')
 
-    serv_dick = edict()
-    serv_dick.size = 0
+    args, _ = parser.parse_known_args()
 
-    serv_dick.res = res
-    #and again, we put size of our dictionary so does client know when to stop reading
-    serv_dick.size = sys.getsizeof(serv_dick)
-
-    serialized = pickle.dumps(serv_dick)
-    conn.send(serialized)
-    conn.close()
+    main(args)
